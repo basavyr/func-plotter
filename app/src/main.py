454cc354d1ed5befc94816d2002694895bc6e3d6
Robert Poenaru <robert.poenaru@protonmail.ch>
2023-06-10 14:25:53 +0300

    add simpson 1/3 and 3/8 methods

--- app/src/main.py
+++ app/src/main.py
@@ -1,4 +1,5 @@
 import maths
+import plotter
 
 
 def func_evolution(func: maths.Functions, interval: tuple):
@@ -14,16 +15,22 @@ def func_evolution(func: maths.Functions, interval: tuple):
     print(
         f'Created the data set for the x-values; {x_data[0]}, {x_data[1]}, ...')
 
-    n_func = [func(x) for x in x_data]
+    y_data = [func(x) for x in x_data]
     print(
-        f'Generated {len(n_func)} numerical values for the function < {func} >')
+        f'Generated {len(y_data)} numerical values for the function < {func} >')
+
+    plotter.plot_data(x_data, y_data)
 
 
 def main():
     """
     - The main workflow of the application
     """
-    func_evolution(maths.Functions.special_sin, (0, 10))
+    # func_evolution(maths.Functions.special_sin, (0, 10))
+    int_13 = maths.Integration.simpson_13(maths.Functions.e_x, (0, 1))
+    int_38 = maths.Integration.simpson_38(maths.Functions.e_x, (0, 1))
+    print(int_13)
+    print(int_38)
 
 
 if __name__ == "__main__":
--- app/src/maths.py
+++ app/src/maths.py
@@ -35,3 +35,83 @@ class Functions:
         """
         func = squared(x)*sin_k_x(x, 3)
         return func
+
+    @staticmethod
+    def e_x(x: float) -> float:
+        """
+        - returns the exponential function for a given argument
+        """
+        exp = np.exp(x)
+        return exp
+
+
+class Integration:
+
+    N_INTERVALS = 100
+
+    @staticmethod
+    def simpson_13(func: 'Functions', interval: tuple) -> float:
+        """
+        - uses Simpson's 1/3 rule to determine the numerical integral of a function
+        """
+        a, b = interval
+        step = np.abs((b-a)/Integration.N_INTERVALS)
+
+        half_range = int(
+            Integration.N_INTERVALS / 2) if Integration.N_INTERVALS % 2 == 0 else int((Integration.N_INTERVALS+1)/2)
+
+        f_a = func(a)
+        f_b = func(b)
+
+        sum1 = 0
+        for i in range(1, half_range+1):
+            x_i = a+(2*i-1)*step
+            sum1 += func(x_i)
+
+        sum2 = 0
+        for i in range(1, half_range):
+            x_i = a+(2*i)*step
+            sum2 += func(x_i)
+
+        simpson_term = f_a+4*sum1+2*sum2+f_b
+
+        return simpson_term*step/3.0
+
+    @staticmethod
+    def simpson_38(func: 'Functions', interval: tuple) -> float:
+        """
+        - uses Simpson's 3/8 rule to determine the numerical integral of a function
+        """
+
+        a, b = interval
+
+        # make sure the number of evaluation intervals is a multiple of 3
+        n_intervals_3 = 3*Integration.N_INTERVALS
+
+        step = np.abs((b-a)/n_intervals_3)
+
+        third_range = int(n_intervals_3/3)
+
+        sum1 = 0
+        for i in range(1, third_range+1):
+            x_i = a+(3*i-3)*step
+            sum1 += func(x_i)
+
+        sum2 = 0
+        for i in range(1, third_range+1):
+            x_i = a+(3*i-2)*step
+            sum2 += func(x_i)
+
+        sum3 = 0
+        for i in range(1, third_range+1):
+            x_i = a+(3*i-1)*step
+            sum3 += func(x_i)
+
+        sum4 = 0
+        for i in range(1, third_range+1):
+            x_i = a+(3*i)*step
+            sum4 += func(x_i)
+
+        simpson_term = sum1+3*sum2+3*sum3+sum4
+
+        return simpson_term*3*step/8
