1869801a1b8e27d858e0a6bec5c0a68c7b503e33
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-03-28 16:37:31 +0200

    add sliders for gamma and omega

--- app/src/_dash_.py
+++ app/src/_dash_.py
@@ -1,6 +1,6 @@
 
 
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc,  Input, Output, callback
 import plotly.express as px
 import pandas as pd
 import numpy as np
@@ -11,28 +11,53 @@ import data_fourier
 app = Dash(__name__)
 
 
-df = data_fourier.df
-
-fig1 = px.scatter(df, x=df.t, y=df.ud, labels={'x': 't', 'ud': 'ud(t)'})
-fig2 = px.line(df, x=df.t, y=df.ud, labels={'x': 't', 'ud': 'ud(t)'})
-
+gamma_step = 0.1
+gamma_values = data_fourier.generate_gamma_values(0, 5, gamma_step)
+omega_step = 5
+omega_values = data_fourier.generate_omega_values(0, 100, omega_step)
 
 app.layout = html.Div([
-    html.Div(children='Please select the value of \gamma'),
+    html.Div(id="gamma-value"),
     html.Br(),
-    dcc.Dropdown(data_fourier.gamma, 'gamma',
-                 id='dropdown-gamma', placeholder='gamma'),
-    html.Div(children='Please select the value of $\omega_0$'),
+    dcc.Slider(min=gamma_values[0], max=gamma_values[-1], step=gamma_step, value=gamma_values[0], id='gamma-slider',
+               tooltip={"placement": "bottom", "always_visible": False}),
+    html.Div(id="omega-value"),
     html.Br(),
-    dcc.Dropdown(data_fourier.omega0,  id='dropdown-omega',
-                 placeholder='omega'),
+    dcc.Slider(min=omega_values[0], max=omega_values[-1], step=omega_step, value=omega_values[0], id='omega-slider',
+               tooltip={"placement": "bottom", "always_visible": False}),
+    html.Br(),
     html.Div(children=data_fourier.HEADER),
     html.Br(),
-    dcc.Graph(
-        id='example-graph2',
-        figure=fig2
-    )
+    dcc.Graph(id='graph-ud')
 ])
+
+
+@callback(Output('gamma-value', 'children'),
+          Input('gamma-slider', 'value'))
+def set_gamma(value):
+    return f'Select the value of gamma. Current value: {value}$'
+
+
+@callback(Output('omega-value', 'children'),
+          Input('omega-slider', 'value'))
+def set_omega(value):
+    return f'Select the value of omega. Current value: {value}$'
+
+
+@callback(
+    Output('graph-ud', 'figure'),
+    Input('gamma-slider', 'value'),
+    Input('omega-slider', 'value'))
+def update_ud(gamma, omega):
+    df = data_fourier.generate_ud_df(gamma, omega)
+
+    fig = px.line(df, x=df.t, y=df.ud, labels={'x': 't', 'ud': 'ud(t)'})
+    fig2 = px.scatter(df, x=df.t, y=df.ud, labels={'x': 't', 'ud': 'ud(t)'})
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/src/data_fourier.py
+++ app/src/data_fourier.py
@@ -15,18 +15,25 @@ def main():
 HEADER = "This is a plot representing the evolution of the under-damped oscillator"
 
 
-gamma = [0.5, 1, 2, 3, 5]
-omega0 = [1, 2, 5, 7, 10]
-t_data = np.linspace(0, 3.14, 75)
-df = pd.DataFrame({
-    "t": t_data,
-    "ud": [fourier.Functions.oscillator_ud(t, gamma[0], omega0[0]) for t in t_data],
-})
+def generate_ud_df(gamma: float, omega: float) -> pd.DataFrame:
+    """
+    - takes the value of gamma (argument that goes into the exponential function) value of omega (argument that controls the cosine function) and creates a data frame object containing all the values for the under-damped oscillator on a fixed interval for `t`
+    """
+    t_data = np.linspace(0, 3.14, 500)
+    df = pd.DataFrame({
+        "t": t_data,
+        "ud": [fourier.Functions.oscillator_ud(t, gamma, omega) for t in t_data],
+    })
+    return df
 
 
-def main():
-    df
+def generate_gamma_values(g_min: float, g_max: float, step: float) -> np.ndarray:
+    n_points = int((g_max-g_min)/step+1)
+    gammas = np.linspace(g_min, g_max, n_points)
+    return gammas
 
 
-if __name__ == '__main__':
-    main()
+def generate_omega_values(w_min: float, w_max: float, step: float) -> np.ndarray:
+    n_points = int((w_max-w_min)/step+1)
+    gammas = np.linspace(w_min, w_max, n_points)
+    return gammas
