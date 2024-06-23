d4ffc559b5a83de0f8c35c9f0e5fd570274a57d0
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-06-23 10:26:16 +0300

    add new plots

--- app/src/_dash_.py
+++ app/src/_dash_.py
@@ -2,6 +2,7 @@
 
 from dash import Dash, html, dcc,  Input, Output, callback
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 
@@ -12,18 +13,19 @@ app = Dash(__name__)
 
 
 gamma_step = 0.1
-gamma_values = data_fourier.generate_gamma_values(0, 5, gamma_step)
 omega_step = 5
+
+gamma_values = data_fourier.generate_gamma_values(0, 5, gamma_step)
 omega_values = data_fourier.generate_omega_values(0, 100, omega_step)
 
 app.layout = html.Div([
     html.Div(id="gamma-value"),
     html.Br(),
-    dcc.Slider(min=gamma_values[0], max=gamma_values[-1], step=gamma_step, value=gamma_values[0], id='gamma-slider',
+    dcc.Slider(min=gamma_values[0], max=gamma_values[-1], step=gamma_step, value=gamma_values[int(len(gamma_values)/2)], id='gamma-slider',
                tooltip={"placement": "bottom", "always_visible": False}),
     html.Div(id="omega-value"),
     html.Br(),
-    dcc.Slider(min=omega_values[0], max=omega_values[-1], step=omega_step, value=omega_values[0], id='omega-slider',
+    dcc.Slider(min=omega_values[0], max=omega_values[-1], step=omega_step, value=omega_values[int(len(omega_values)/2)], id='omega-slider',
                tooltip={"placement": "bottom", "always_visible": False}),
     html.Br(),
     html.Div(children=data_fourier.HEADER),
@@ -35,26 +37,56 @@ app.layout = html.Div([
 @callback(Output('gamma-value', 'children'),
           Input('gamma-slider', 'value'))
 def set_gamma(value):
-    return f'Select the value of gamma. Current value: {value}$'
+    return f'Select the value of ɣ. Current value: ɣ={value}'
 
 
 @callback(Output('omega-value', 'children'),
           Input('omega-slider', 'value'))
 def set_omega(value):
-    return f'Select the value of omega. Current value: {value}$'
+    return f'Select the value of ω. Current value: ω={value}'
 
 
 @callback(
     Output('graph-ud', 'figure'),
     Input('gamma-slider', 'value'),
     Input('omega-slider', 'value'))
-def update_ud(gamma, omega):
-    df = data_fourier.generate_ud_df(gamma, omega)
+def update_ud_plot(gamma, omega):
+    df_line = data_fourier.generate_ud_df(1500, gamma, omega).th
+    df_scatter = data_fourier.generate_ud_df(100, gamma, omega).exp
 
-    fig = px.line(df, x=df.t, y=df.ud, labels={'x': 't', 'ud': 'ud(t)'})
-    fig2 = px.scatter(df, x=df.t, y=df.ud, labels={'x': 't', 'ud': 'ud(t)'})
+    # line_plot = px.line(df_line, x=df_line.t, y=df_line.ud,
+    #                     color_discrete_sequence=['black'])
+    # scatter_plot = px.scatter(
+    #     df_scatter, x=df_scatter.t, y=df_scatter.ud, color_discrete_sequence=['red'])
 
-    fig.update_layout(transition_duration=500)
+    # go_fig = go.Figure(data=line_plot.data + scatter_plot.data)
+
+    # go_fig.update_layout(transition_duration=500,
+    #                      title="Under-damped oscillator",
+    #                      xaxis_title="t",
+    #                      yaxis_title="ud(t)",
+    #                      legend_title="Under-damped oscillator"
+    #                      )
+
+    # return go_fig
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=df_scatter.t, y=df_scatter.ud,
+        name="Name of Trace 1"
+    ))
+    fig.add_trace(go.Line(
+        x=df_line.t, y=df_line.ud,
+        name="Name of Trace 1"
+    ))
+
+    fig.update_layout(
+        title="Plot Title",
+        xaxis_title="X Axis Title",
+        yaxis_title="Y Axis Title",
+        legend_title="Legend Title"
+    )
 
     return fig
 
--- app/src/data_fourier.py
+++ app/src/data_fourier.py
@@ -15,16 +15,34 @@ def main():
 HEADER = "This is a plot representing the evolution of the under-damped oscillator"
 
 
-def generate_ud_df(gamma: float, omega: float) -> pd.DataFrame:
+def generate_ud_df(n_points: int, gamma: float, omega: float) -> pd.DataFrame:
     """
     - takes the value of gamma (argument that goes into the exponential function) value of omega (argument that controls the cosine function) and creates a data frame object containing all the values for the under-damped oscillator on a fixed interval for `t`
     """
-    t_data = np.linspace(0, 3.14, 500)
+    t_data = np.linspace(0, 3.14, n_points)
     df = pd.DataFrame({
-        "t": t_data,
-        "ud": [fourier.Functions.oscillator_ud(t, gamma, omega) for t in t_data],
+        "exp": {
+            "t": t_data,
+            "ud": [fourier.Functions.oscillator_ud(t, gamma, omega) for t in t_data]
+        },
+        "th": {
+            "t": t_data,
+            "ud": [generate_th_value(fourier.Functions.oscillator_ud(t, gamma, omega), 10) for t in t_data]
+        }
     })
     return df
+
+
+def generate_th_value(exp_value: float, delta: int) -> float:
+    """
+    - generate a *theoretical* value based on an experimental one.
+    - the degree of deviation from the experiment will be provided by a "delta" scale, measuring how much the theoretical value will deviate from the experimental one
+
+    - th value is evaluated using the following approach:
+        `th = exp+ rand(0,1)*delta`
+    """
+    d = np.random.random()
+    return exp_value+(d/delta)*exp_value
 
 
 def generate_gamma_values(g_min: float, g_max: float, step: float) -> np.ndarray:
